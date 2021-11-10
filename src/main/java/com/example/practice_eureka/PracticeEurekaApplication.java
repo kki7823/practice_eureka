587b9a4591db784d78fb180762041b59a68cec40
kki7823 <kjd55399997@gmail.com>
2021-11-11 06:49:18 +0900

    Initial commit

---
+++ src/main/java/com/example/practice_eureka/PracticeEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.example.practice_eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeEurekaApplication.class, args);
+	}
+
+}
